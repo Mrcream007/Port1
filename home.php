d11a5261c972eae69ade95405b9afaae69f34e2f
Mrcream007 <okochachuka@gmail.com>
2023-05-12 19:03:32 +0100

    edits3

--- home.php
+++ home.php
@@ -38,6 +38,49 @@
 
 
 
+    <!-- footer start -->
+
+    <section class="footer">
+ 
+        <div class="box-container">
+
+            <div class="box">
+                <h3>quick links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i> home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+                <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+                <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+            </div>
+
+            <div class="box">
+                <h3>extra links</h3>
+                <a href="#"><i class="fas fa-angle-right"></i> ask questions</a>
+                <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+                <a href="#"><i class="fas fa-angle-right"></i> privacy</a>
+                <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+            </div>
+
+            <div class="box">
+                <h3>contact info</h3>
+                <a href="#"><i class="fas fa-angle-right"></i> +123-456-7890</a>
+                <a href="#"><i class="fas fa-phone"></i> +222-333-4444</a>
+                <a href="#"><i class="fas fa-envelope"></i> johndoe@gmail.com</a>
+                <a href="#"><i class="fas fa-map"></i> aberdeen, Scotland - AB24</a>
+            </div>
+
+            <div class="box">
+                <h3>follow us</h3>
+                <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+                <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+                <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+                <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+            </div>
+
+        </div>
+
+    </section>
+
+    <!-- footer ends -->
 
 
     <!-- swiper js link -->
