39fe606145dee87f13b78229a35e94055e72f939
Mrcream007 <okochachuka@gmail.com>
2023-06-08 15:48:39 +0100

    Chatbot

---
+++ bot.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chatbot</title>
+    <!-- css link file -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.7/css/all.css">
+    <!-- font awesome link -->
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="title">Online Chatbot</div>
+        <div class="form">
+            <div class="bot-inbox inbox">
+                <div class="icon">
+                <i class=""></i>
+                </div>
+                <div class="msg-header">
+                    <p>Welcome, how may I be of service to you?</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
