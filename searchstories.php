cee9ace7ab70df29b7b9ec1cc1542dec9fdefd3f
Mrcream007 <okochachuka@gmail.com>
2023-07-09 18:38:02 +0100

    chatbot php

--- searchstories.php
+++ searchstories.php
@@ -37,7 +37,7 @@ if (isset($_SESSION['brend_id'])) {
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 
-  <link rel="stylesheet" href="assets/css/search1.css">
+  <link rel="stylesheet" href="css/style1.css">
   <!--<link rel="stylesheet" href="css/style.css">-->
 
   <link rel="stylesheet" href="/assets/css/unsemantic-grid-responsive-tablet.css">
@@ -128,5 +128,6 @@ else:
     </div>
 
     <a href="homepage1.php">Homepage</a>
+
 </body>
 </html>
