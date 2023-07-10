1ef8b5dcfad492f7529b97b1f8eacb75f0e1c305
Mrcream007 <okochachuka@gmail.com>
2023-07-10 12:50:39 +0100

    final code for chatbot, javascript, ajax

--- bot.php
+++ bot.php
@@ -82,9 +82,12 @@
                 $.ajax({
                     url: 'message.php',
                     type: 'POST',
-                    data: 'text ='+$value,
+                    data: 'text='+$value,
                     success: function(result){
-
+                        $replay = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-fire"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
+                        $(".form").append($replay);
+                        // when chat goes down the scroll bar automatically comes to the bottom
+                        $(".form").scrollTop($(".form")[0].scrollHeight);
                     }
                 });
             });
